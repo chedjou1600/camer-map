51688bbc05c1e4e62bd05ca4cc83b81871de4c5a
Hilary29 <keubouhilary@gmail.com>
2024-12-06 12:27:18 +0100

    Adding Map section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,16 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 py-8 mt-12">
+    <footer className="bg-gray-100 py-8">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-8">
         {/* Footer Links */}
         <div className="flex flex-col md:flex-row gap-6 text-gray-600 text-sm">
-          <a href="#about" className="hover:text-green-500">
-            About Us
-          </a>
-          <a href="#contact" className="hover:text-green-500">
-            Contact
-          </a>
           <a href="#privacy" className="hover:text-green-500">
             Privacy Policy
           </a>
@@ -22,7 +16,7 @@ const Footer = () => {
 
         {/* Copyright */}
         <p className="text-gray-600 mt-4 md:mt-0 text-sm">
-          &copy; {new Date().getFullYear()} AgriNet. All rights reserved.
+          &copy; {new Date().getFullYear()} . All rights reserved.
         </p>
       </div>
     </footer>
