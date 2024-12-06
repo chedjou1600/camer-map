8dd411a99a8ee3dae8e8a02bdf3c9f6f26d69fbf
Hilary29 <keubouhilary@gmail.com>
2024-12-06 07:38:57 +0100

    First commit

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='align-center justify-center mx-auto '>
+        page
+      
+    </div>
+  )
+}
+
+export default page
